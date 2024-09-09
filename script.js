a35de2fa2452db61aa05cd9bd8a0f6d054bfac14
KAUZ1NNN <98913894+KAUZ1NNN@users.noreply.github.com>
2024-09-09 13:56:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('buyButton').addEventListener('click', function() {
+    window.location.href = 'https://seulinkdepagamentonaKiwify.com'; // Substitua pelo link da sua página de pagamento no Kiwify
+});
